4097d39abc570414c2826fbff3e1cce481d0fe03
daniela6tres <daniela@6tres.com.br>
2020-05-10 18:11:31 -0300

    exercicio de layout das primeiras telas de login

---
+++ AppBaseXf/AppBaseXf/AppBaseXf/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppBaseXf.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBaseXf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppBaseXf/AppBaseXf/AppBaseXf/Pages/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBaseXf.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Logar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Cadastro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
